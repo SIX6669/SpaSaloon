99ec2ce8a2c1036b1595ef31f4e3754111bec2fb
SIX <simon.494.perrez@gmail.com>
2025-04-04 16:35:34 -0300

    first commit

---
+++ SpaSaloon/src/componentes/buttons.jsx
@@ -0,0 +1,15 @@
+// eslint-disable-next-line react-refresh/only-export-components
+function MyButton() {
+  return (
+    <button>Soy un botón</button>
+  );
+}
+
+
+function texto() {
+  return (
+    <p>Texto de ejemplo</p>
+  );
+}
+
+export { texto, MyButton };
