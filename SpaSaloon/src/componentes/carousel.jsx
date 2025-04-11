bed75b9d5465bf311631d264d2c74062803259eb
SIX <simon.494.perrez@gmail.com>
2025-04-11 16:38:28 -0300

    m

--- SpaSaloon/src/componentes/carousel.jsx
+++ SpaSaloon/src/componentes/carousel.jsx
@@ -3,9 +3,12 @@ import Slider from 'react-slick';
 import slide1 from '../assets/slide1.jpg';
 import slide2 from '../assets/slide2.jpg';
 import slide3 from '../assets/slide3.jpg';
+import slide4 from '../assets/slide4.jpg';
+import slide5 from '../assets/slide5.jpg';
+
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-
+import '../styles/carousel.css';
 function Carousel() {
      const settings = {
         dots: true,
@@ -16,7 +19,7 @@ function Carousel() {
     };
 
     return (
-      <Slider {...settings}>
+      <Slider classname="slider" {...settings}>
       <div className="slide">
         <img src={slide1} alt="Slide 1" />
       </div>
@@ -25,6 +28,12 @@ function Carousel() {
       </div>
       <div className="slide">
         <img src={slide3} alt="Slide 3" />
+      </div>
+      <div className="slide">
+        <img src={slide4} alt="Slide 4" />
+      </div>
+      <div className="slide">
+        <img src={slide5} alt="Slide 5" />
       </div>
     </Slider>
     
